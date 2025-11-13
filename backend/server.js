d9a9a5462ddd8546063df3d9206bdb00b1bc68a9
Bala0445 <balamurugan1028@gmail.com>
2025-11-13 11:33:24 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/movies", movieRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
